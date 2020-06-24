56fab3839cc7eb7abb3fdda76c2daa6870496600
ZEBERKO Camille <camille.zeberko@soprahr.com>
2020-06-24 09:28:27 +0200

    Remise à niveau 24/06

---
+++ app/app.js
@@ -0,0 +1,14 @@
+function onloadhtml(params) {
+    console.log('%cVoici les params :%s','color:blue;font-weight:900;font-size:30pt',params)
+    return '';
+}
+// var body=document.querySelector('body');
+// body.addEventListener('load',onloadhtml);
+var domUser=document.querySelectorAll('#tchat-users>.user');
+domUser.forEach(function(element){
+    element.addEventListener('click',function(evt) {
+        console.log(evt);
+        console.log(evt.target.innerHTML);
+        // evt.target.innerHTML="Je suis pas ZOIDBERG"
+    })
+});
